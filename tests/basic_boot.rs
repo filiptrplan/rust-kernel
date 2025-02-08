647295705018f668589a7fb61c0db330ded05616
Filip Trplan <filip@trplan.si>
2025-02-08 22:32:59 +0100

    integration and unit tests

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -7,25 +7,18 @@
 use core::panic::PanicInfo;
 use trplan_os::println;
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
 
-    #[cfg(test)]
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
     test_main();
 
     loop {}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     trplan_os::test_panic_handler(info)
