b51062df3cce68135fac19d1ca9c6ef1e566c246
Filip Trplan <filip@trplan.si>
2025-02-09 19:41:10 +0100

    double fault handler

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,6 @@ pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
     trplan_os::init();
-    x86_64::instructions::interrupts::int3();
 
     #[cfg(test)]
     test_main();
